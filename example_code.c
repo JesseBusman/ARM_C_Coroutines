3049ca5be319f7204c47a2bb5beea734147b3cce
Jesse Busman <jesse@jesbus.com>
2019-01-19 19:32:39 +0100

    Create example_code.c

---
+++ example_code.c
@@ -0,0 +1,64 @@
+#include "coroutines.h"
+
+// This coroutine will return 1, 2, 3, then stop itself.
+Coroutine* OneTwoThree(uint32_t runValue, Coroutine* co)
+{
+  CoYield(1, co);
+  CoYield(2, co);
+  CoReturn(3, co);
+}
+
+// CounterCoroutine consumes its value and adds it to an internal running count.
+// It will yield the current running count on every run.
+Coroutine* CounterCoroutine(uint32_t runValue, Coroutine* co)
+{
+	uint32_t runningCount;
+	runningCount = value;
+	while (1)
+	{
+		runningCount += CoYield(runningCount, co);
+	}
+}
+
+// FibonacciCoroutine does not consume any value.
+// It will yield the next fibonacci number on every run.
+Coroutine* FibonacciCoroutine(uint32_t runValue, Coroutine* co)
+{
+	uint32_t a, b, temp;
+	a = 1;
+	b = 0;
+	while(1)
+	{
+		CoYield(b, co);
+		temp = a + b;
+		b = a;
+		a = temp;
+	}
+}
+
+void Test()
+{
+	Coroutine* gen;
+  Coroutine* adder;
+  uint32_t i, yieldValue;
+	
+	gen = CoInit(&FibonacciCoroutine, 64);
+  adder = CoInit(&CounterCoroutine, 64);
+  
+  for (i=0; i<25; i++)
+	{
+    yieldValue = CoRun(0, gen);
+		printf("Fibonacci number: %i\r\n", yieldValue);
+		printf("Sum so far: %i\r\n", CoRun(yieldValue, adder));
+	}
+	CoKill(gen);
+  CoKill(adder);
+  
+  
+  gen = CoInit(&OneTwoThree, 64);
+  while (!CoHasFinished(gen))
+  {
+    printf("%i\r\n", CoRun(0, gen));
+  }
+  CoEnd(gen);
+}
